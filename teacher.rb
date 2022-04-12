bd1c55dbc6eee8f25ddca96e8f98f0a5d2093698
Melanie Arellano <melaniesigrid@protonmail.com>
2022-04-11 18:49:40 -0600

    Set parent permission to true, set name of of_age method to is_of_age

--- teacher.rb
+++ teacher.rb
@@ -1,5 +1,5 @@
 class Teacher < person
-  def initialize(specialization, _parent_permission, name = 'Unknown', age: true)
+  def initialize(specialization, parent_permission: true, name = 'Unknown', age: true)
     super(age, parent_permission, name)
     @specialization = specialization
   end
