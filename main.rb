720e4d7483a78b395b01dc6df4be9e8c446e2aaa
Melanie Arellano <melaniesigrid@protonmail.com>
2022-04-14 20:45:12 -0600

    Add permissions

--- main.rb
+++ main.rb
@@ -5,19 +5,19 @@ class Main
   def display_choices
     puts
     puts 'Please choose an option by entering a number:'
-    puts '1 - List all books.'
-    puts '2 - List all people.'
+    puts '1 - List all books. 📖'
+    puts '2 - List all people. 👤'
     puts '3 - Create a person.'
     puts '4 - Create a book.'
     puts '5 - Create a rental.'
     puts '6 - List all rentals for a given person ID.'
-    puts '7 - Exit.'
+    puts '7 - Exit. ❌'
   end
 
   def main
     app = App.new
     selection = nil
-    puts 'Welcome to the School Library App!'
+    puts 'Welcome to the School Library App! 📖'
     while selection != 'Exit'
       display_choices
       selection = app.select_choice
